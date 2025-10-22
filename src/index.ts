e1c0553803f33457ce1f7e5b15d708494132714b
Leonardo Scardanzan <leonardo.scardanzan@ext.vaadin.com>
2025-10-22 16:41:09 -0300

    fix: exports generateImage to enable manually triggering the image generation

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { tabletDemo } from "./wacomUtil";
+import { tabletDemo, generateImage } from "./wacomUtil";
 import "./sigPadStyle.css";
 
 export { tabletDemo as showWacomSignPad };
+export {generateImage};
